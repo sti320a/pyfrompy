a0841d1b0aa679aabebbc6aca295514dca628498
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-16 01:01:26 +0900

    Create python script for making python script from python script

---
+++ origin.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime
+
+cwd = os.getcwd()
+filename = f"{cwd}/{datetime.now().strftime('%Y%m%d%H%M%S')}.py"
+
+
+def mkfile(filename: str):
+    with open(filename, 'w') as f:
+        f.write("print('Hello, world')")
+
+
+if __name__ == '__main__':
+    mkfile(filename)
+    with open(filename, 'r', encoding='utf-8') as f:
+        pyscript = f.read()
+        exec(pyscript)
