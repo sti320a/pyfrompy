1b0f7575e4a520cfa7609715276c23649f4187f1
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-17 00:34:25 +0900

    Refactor

--- origin.py
+++ origin.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 current_dir = os.getcwd()
 timestamp_now = datetime.now().strftime('tmp_%Y%m%d%H%M%S')
-filename = f"{current_dir}/{timestamp_now}.py"
+filename = f'{current_dir}/{timestamp_now}.py'
 
 
 def mkfile(filename: str):
     with open(filename, 'w') as f:
-        f.write("print('Hello, world')\n")
+        f.write('print("Hello, world")\n')
 
 
 if __name__ == '__main__':
